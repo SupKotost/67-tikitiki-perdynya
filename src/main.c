88a0aee358236b21c2b0d0bc984d2796af2e597a
SupKotost <egor79757@gmail.com>
2026-08-22 12:25:48 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,9 +2,16 @@
 #include <pspdebug.h>
 #include <pspdisplay.h>
 #include <pspctrl.h>
+#include <stdlib.h>
+#include <time.h>
 
-PSP_MODULE_INFO("Nikitu v0.0.3", 0, 1, 0);
+PSP_MODULE_INFO("Nikitu v0.0.4", 0, 1, 0);
 PSP_MAIN_THREAD_ATTR(PSP_THREAD_ATTR_USER);
+
+#define FIELD_LEFT   1
+#define FIELD_RIGHT  38
+#define FIELD_TOP    1
+#define FIELD_BOTTOM 18
 
 int exit_callback(int arg1, int arg2, void *common)
 {
@@ -44,25 +51,124 @@ int setup_callbacks(void)
     return thid;
 }
 
+/*
+    Направления Орешника:
+
+    0 = слева -> направо
+    1 = справа -> налево
+    2 = сверху -> вниз
+    3 = снизу -> вверх
+*/
+
+void spawn_missile(
+    int *missileX,
+    int *missileY,
+    int *direction,
+    int *missileSpeed
+)
+{
+    *direction = rand() % 4;
+
+    *missileSpeed = 1 + rand() % 3;
+
+    if (*direction == 0)
+    {
+        *missileX = FIELD_LEFT;
+        *missileY = FIELD_TOP +
+                     rand() % (FIELD_BOTTOM - FIELD_TOP + 1);
+    }
+    else if (*direction == 1)
+    {
+        *missileX = FIELD_RIGHT;
+        *missileY = FIELD_TOP +
+                     rand() % (FIELD_BOTTOM - FIELD_TOP + 1);
+    }
+    else if (*direction == 2)
+    {
+        *missileX = FIELD_LEFT +
+                    rand() % (FIELD_RIGHT - FIELD_LEFT + 1);
+        *missileY = FIELD_TOP;
+    }
+    else
+    {
+        *missileX = FIELD_LEFT +
+                    rand() % (FIELD_RIGHT - FIELD_LEFT + 1);
+        *missileY = FIELD_BOTTOM;
+    }
+}
+
+int missile_inside_field(int x, int y)
+{
+    if (x < FIELD_LEFT || x > FIELD_RIGHT)
+        return 0;
+
+    if (y < FIELD_TOP || y > FIELD_BOTTOM)
+        return 0;
+
+    return 1;
+}
+
+void game_over(SceCtrlData *pad)
+{
+    pspDebugScreenClear();
+
+    pspDebugScreenPrintf("\n\n");
+    pspDebugScreenPrintf("========================================\n");
+    pspDebugScreenPrintf("              GAME OVER\n");
+    pspDebugScreenPrintf("========================================\n\n");
+
+    pspDebugScreenPrintf("THE ORESHNIK GOT YOU.\n\n");
+    pspDebugScreenPrintf("PRESS START TO RETURN\n");
+
+    while (1)
+    {
+        sceCtrlReadBufferPositive(pad, 1);
+
+        if (pad.Buttons & PSP_CTRL_START)
+            return;
+
+        sceDisplayWaitVblankStart();
+    }
+}
+
 void game(void)
 {
     SceCtrlData pad;
 
-    int playerX = 30;
+    int playerX = 20;
     int playerY = 10;
 
-    int missileX = 1;
-    int missileY = 10;
+    int missileX;
+    int missileY;
+    int missileDirection;
+    int missileSpeed;
 
-    int missileSpeed = 2;
     int frame = 0;
+
+    int obstacleX = 0;
+    int obstacleY = 0;
+    int obstacleActive = 0;
+
+    int obstacleTimer = 0;
+    int obstacleDuration = 0;
+
+    int nextObstacleTimer = 100;
+
+    srand((unsigned int)time(NULL));
+
+    spawn_missile(
+        &missileX,
+        &missileY,
+        &missileDirection,
+        &missileSpeed
+    );
 
     while (1)
     {
         pspDebugScreenClear();
 
         pspDebugScreenPrintf("\n");
-        pspDebugScreenPrintf("              NIKITU v0.0.3\n");
+        pspDebugScreenPrintf("             NIKITU v0.0.4\n");
         pspDebugScreenPrintf("========================================\n\n");
 
         for (int y = 0; y < 20; y++)
@@ -70,86 +176,204 @@ void game(void)
             for (int x = 0; x < 40; x++)
             {
                 if (x == playerX && y == playerY)
+                {
                     pspDebugScreenPrintf("@");
-                else if (x == missileX && y == missileY)
+                }
+                else if (
+                    x == missileX &&
+                    y == missileY &&
+                    missile_inside_field(missileX, missileY)
+                )
+                {
                     pspDebugScreenPrintf("O");
-                else if (x == 0 || x == 39 || y == 0 || y == 19)
+                }
+                else if (
+                    obstacleActive &&
+                    x == obstacleX &&
+                    y == obstacleY
+                )
+                {
                     pspDebugScreenPrintf("#");
+                }
+                else if (
+                    x == 0 ||
+                    x == 39 ||
+                    y == 0 ||
+                    y == 19
+                )
+                {
+                    pspDebugScreenPrintf("#");
+                }
                 else
+                {
                     pspDebugScreenPrintf(" ");
+                }
             }
 
             pspDebugScreenPrintf("\n");
         }
 
         pspDebugScreenPrintf("\n");
-        pspDebugScreenPrintf("UP/DOWN - DODGE\n");
+        pspDebugScreenPrintf("D-PAD - MOVE\n");
         pspDebugScreenPrintf("START - MENU\n");
 
         sceCtrlReadBufferPositive(&pad, 1);
 
+        /* Управление игроком */
+
+        if (pad.Buttons & PSP_CTRL_LEFT)
+        {
+            if (playerX > FIELD_LEFT)
+                playerX--;
+
+            sceKernelDelayThread(50000);
+        }
+
+        if (pad.Buttons & PSP_CTRL_RIGHT)
+        {
+            if (playerX < FIELD_RIGHT)
+                playerX++;
+
+            sceKernelDelayThread(50000);
+        }
+
         if (pad.Buttons & PSP_CTRL_UP)
         {
-            if (playerY > 1)
+            if (playerY > FIELD_TOP)
                 playerY--;
 
-            sceKernelDelayThread(80000);
+            sceKernelDelayThread(50000);
         }
 
         if (pad.Buttons & PSP_CTRL_DOWN)
         {
-            if (playerY < 18)
+            if (playerY < FIELD_BOTTOM)
                 playerY++;
 
-            sceKernelDelayThread(80000);
+            sceKernelDelayThread(50000);
         }
 
         if (pad.Buttons & PSP_CTRL_START)
             return;
 
+        /* Движение Орешника */
+
         frame++;
 
         if (frame >= missileSpeed)
         {
-            missileX++;
+            frame = 0;
+
+            if (missileDirection == 0)
+                missileX++;
+
+            else if (missileDirection == 1)
+                missileX--;
+
+            else if (missileDirection == 2)
+                missileY++;
+
+            else if (missileDirection == 3)
+                missileY--;
+        }
+
+        /*
+            Если Орешник вылетел за пределы поля,
+            создаём новый с другой случайной траекторией.
+        */
+
+        if (
+            missileX < FIELD_LEFT ||
+            missileX > FIELD_RIGHT ||
+            missileY < FIELD_TOP ||
+            missileY > FIELD_BOTTOM
+        )
+        {
+            spawn_missile(
+                &missileX,
+                &missileY,
+                &missileDirection,
+                &missileSpeed
+            );
+
             frame = 0;
         }
 
-        if (missileX == playerX && missileY == playerY)
+        /* Столкновение с Орешником */
+
+        if (
+            playerX == missileX &&
+            playerY == missileY
+        )
         {
-            pspDebugScreenClear();
+            game_over(&pad);
+            return;
+        }
 
-            pspDebugScreenPrintf("\n\n");
-            pspDebugScreenPrintf("========================================\n");
-            pspDebugScreenPrintf("              GAME OVER\n");
-            pspDebugScreenPrintf("========================================\n\n");
-            pspDebugScreenPrintf("THE ORESHNIK GOT YOU.\n\n");
-            pspDebugScreenPrintf("PRESS START TO RETURN\n");
+        /* Таймер появления препятствия */
 
-            while (1)
+        if (!obstacleActive)
+        {
+            nextObstacleTimer--;
+
+            if (nextObstacleTimer <= 0)
             {
-                sceCtrlReadBufferPositive(&pad, 1);
+                obstacleActive = 1;
 
-                if (pad.Buttons & PSP_CTRL_START)
-                    return;
+                obstacleX =
+                    FIELD_LEFT +
+                    rand() % (FIELD_RIGHT - FIELD_LEFT + 1);
 
-                sceDisplayWaitVblankStart();
+                obstacleY =
+                    FIELD_TOP +
+                    rand() % (FIELD_BOTTOM - FIELD_TOP + 1);
+
+                /*
+                    Не создаём препятствие
+                    прямо на игроке.
+                */
+
+                while (
+                    obstacleX == playerX &&
+                    obstacleY == playerY
+                )
+                {
+                    obstacleX =
+                        FIELD_LEFT +
+                        rand() % (FIELD_RIGHT - FIELD_LEFT + 1);
+
+                    obstacleY =
+                        FIELD_TOP +
+                        rand() % (FIELD_BOTTOM - FIELD_TOP + 1);
+                }
+
+                obstacleDuration = 60 + rand() % 120;
+                obstacleTimer = obstacleDuration;
+            }
+        }
+        else
+        {
+            obstacleTimer--;
+
+            if (obstacleTimer <= 0)
+            {
+                obstacleActive = 0;
+
+                nextObstacleTimer =
+                    80 + rand() % 180;
             }
         }
 
-        if (missileX >= 39)
+        /* Столкновение с препятствием */
+
+        if (
+            obstacleActive &&
+            playerX == obstacleX &&
+            playerY == obstacleY
+        )
         {
-            missileX = 1;
-
-            /* Меняем высоту следующего Орешника */
-            missileY++;
-
-            if (missileY >= 18)
-                missileY = 2;
-
-            /* Постепенно ускоряем */
-            if (missileSpeed > 1)
-                missileSpeed--;
+            game_over(&pad);
+            return;
         }
 
         sceDisplayWaitVblankStart();
@@ -174,7 +398,7 @@ int main(void)
 
         pspDebugScreenPrintf("\n\n");
         pspDebugScreenPrintf("================================\n");
-        pspDebugScreenPrintf("            NIKITU v0.0.3\n");
+        pspDebugScreenPrintf("            NIKITU v0.0.4\n");
         pspDebugScreenPrintf("================================\n\n");
 
         if (selected == 0)
