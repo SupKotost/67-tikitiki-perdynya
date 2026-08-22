aeb3fe43ddbbe47aa0f50daf17f9a53550fde12e
SupKotost <egor79757@gmail.com>
2026-08-22 12:15:16 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include <pspdisplay.h>
 #include <pspctrl.h>
 
-PSP_MODULE_INFO("Nikitu", 0, 1, 0);
+PSP_MODULE_INFO("Nikitu v0.0.3", 0, 1, 0);
 PSP_MAIN_THREAD_ATTR(PSP_THREAD_ATTR_USER);
 
 int exit_callback(int arg1, int arg2, void *common)
@@ -15,7 +15,9 @@ int exit_callback(int arg1, int arg2, void *common)
 int callback_thread(SceSize args, void *argp)
 {
     int cbid = sceKernelCreateCallback(
-        "Exit Callback", exit_callback, NULL
+        "Exit Callback",
+        exit_callback,
+        NULL
     );
 
     if (cbid >= 0)
@@ -46,24 +48,31 @@ void game(void)
 {
     SceCtrlData pad;
 
-    int playerX = 15;
+    int playerX = 30;
     int playerY = 10;
+
+    int missileX = 1;
+    int missileY = 10;
+
+    int missileSpeed = 2;
+    int frame = 0;
 
     while (1)
     {
         pspDebugScreenClear();
 
         pspDebugScreenPrintf("\n");
-        pspDebugScreenPrintf("             NIKITU\n");
-        pspDebugScreenPrintf("================================\n\n");
+        pspDebugScreenPrintf("              NIKITU v0.0.3\n");
+        pspDebugScreenPrintf("========================================\n\n");
 
-        /* Игровое поле */
         for (int y = 0; y < 20; y++)
         {
             for (int x = 0; x < 40; x++)
             {
                 if (x == playerX && y == playerY)
                     pspDebugScreenPrintf("@");
+                else if (x == missileX && y == missileY)
+                    pspDebugScreenPrintf("O");
                 else if (x == 0 || x == 39 || y == 0 || y == 19)
                     pspDebugScreenPrintf("#");
                 else
@@ -73,26 +82,11 @@ void game(void)
             pspDebugScreenPrintf("\n");
         }
 
-        pspDebugScreenPrintf("\nD-PAD - MOVE");
-        pspDebugScreenPrintf("\nSTART - MENU");
+        pspDebugScreenPrintf("\n");
+        pspDebugScreenPrintf("UP/DOWN - DODGE\n");
+        pspDebugScreenPrintf("START - MENU\n");
 
         sceCtrlReadBufferPositive(&pad, 1);
-
-        if (pad.Buttons & PSP_CTRL_LEFT)
-        {
-            if (playerX > 1)
-                playerX--;
-
-            sceKernelDelayThread(80000);
-        }
-
-        if (pad.Buttons & PSP_CTRL_RIGHT)
-        {
-            if (playerX < 38)
-                playerX++;
-
-            sceKernelDelayThread(80000);
-        }
 
         if (pad.Buttons & PSP_CTRL_UP)
         {
@@ -112,6 +106,51 @@ void game(void)
 
         if (pad.Buttons & PSP_CTRL_START)
             return;
+
+        frame++;
+
+        if (frame >= missileSpeed)
+        {
+            missileX++;
+            frame = 0;
+        }
+
+        if (missileX == playerX && missileY == playerY)
+        {
+            pspDebugScreenClear();
+
+            pspDebugScreenPrintf("\n\n");
+            pspDebugScreenPrintf("========================================\n");
+            pspDebugScreenPrintf("              GAME OVER\n");
+            pspDebugScreenPrintf("========================================\n\n");
+            pspDebugScreenPrintf("THE ORESHNIK GOT YOU.\n\n");
+            pspDebugScreenPrintf("PRESS START TO RETURN\n");
+
+            while (1)
+            {
+                sceCtrlReadBufferPositive(&pad, 1);
+
+                if (pad.Buttons & PSP_CTRL_START)
+                    return;
+
+                sceDisplayWaitVblankStart();
+            }
+        }
+
+        if (missileX >= 39)
+        {
+            missileX = 1;
+
+            /* Меняем высоту следующего Орешника */
+            missileY++;
+
+            if (missileY >= 18)
+                missileY = 2;
+
+            /* Постепенно ускоряем */
+            if (missileSpeed > 1)
+                missileSpeed--;
+        }
 
         sceDisplayWaitVblankStart();
     }
@@ -135,7 +174,7 @@ int main(void)
 
         pspDebugScreenPrintf("\n\n");
         pspDebugScreenPrintf("================================\n");
-        pspDebugScreenPrintf("            NIKITU\n");
+        pspDebugScreenPrintf("            NIKITU v0.0.3\n");
         pspDebugScreenPrintf("================================\n\n");
 
         if (selected == 0)
